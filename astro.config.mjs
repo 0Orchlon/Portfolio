d24912740f2beaddfd38c1c092b64e31749511ea
0Orchlon <sw22d045@mandakh.edu.mn>
2026-05-11 18:38:37 +0800

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://0Orchlon.github.io',
+  base: '/Portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
